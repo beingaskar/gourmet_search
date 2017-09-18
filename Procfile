a59de95f7cd24a7f0bba699c52f28b9fe904b9b3
Askar Ali <ali.askar@dudegenie.com>
2017-09-18 23:02:53 +0530

    Added requirements and heroku config files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gourmet_search.wsgi --log-file -
--- gourmet_search/settings.py
+++ gourmet_search/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ti!y&a+0@(s%e!lq9fdk7$1iy$0cz4&g=t=kun=b_i*j*@tx-)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,6 +103,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 ASSETS_DIR = os.path.abspath(os.path.join(BASE_DIR, 'assets')) + "\\"
 
