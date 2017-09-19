b696249561668ed06bb510c85cb3e24f01845041
Askar Ali <ali.askar@dudegenie.com>
2017-09-20 01:35:09 +0530

    Test case added - File format check

--- gourmet/tests.py
+++ gourmet/tests.py
@@ -9,11 +9,9 @@ class GourmetSearchTest(TestCase):
 
     def test_reviews_search_post(self):
         url = reverse('gourmet:reviews_search')
-
         # Positive scenario
         response = self.client.post(url, {"query": "admittedly coffee"})
         self.assertEqual(response.status_code, 200)
-
         # Negative scenario
         response = self.client.post(url)
         self.assertEqual(response.status_code, 400)
@@ -23,10 +21,14 @@ class GourmetSearchTest(TestCase):
         file_path_valid = settings.FILES["REVIEWS"]["INDEX_REVIEW_LEVEL"]
         data = load_json_data(file_path_valid)
         self.assertEqual(type(data), dict)
-
         # Negative scenario
         file_path_invalid = "/test/test.txt"
         data = load_json_data(file_path_invalid)
         self.assertIsNone(data)
+
+    def test_static_data(self):
+        self.assertEqual(type(settings.REVIEWS_DATA), list)
+        self.assertEqual(type(settings.REVIEWS_INDEX_TERM_LEVEL), dict)
+        self.assertEqual(type(settings.REVIEWS_INDEX_REVIEW_LEVEL), dict)
 
 
